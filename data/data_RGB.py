4e5cc579122d2f36bb26dfd87a731a3b82068f5d
xuhangc <yc17491@umac.mo>
2023-08-24 14:39:44 +0800

    edit

--- data/data_RGB.py
+++ data/data_RGB.py
@@ -1,12 +1,12 @@
 import os
 from .dataset_RGB import DataReader
 
+
 def check_folder(img_dir, inp, tar, mode):
-    assert os.path.exists(os.paht.join(img_dir, mode, inp))
-    assert os.path.exists(os.paht.join(img_dir, mode, tar))
+    assert os.path.exists(os.path.join(img_dir, mode, inp))
+    assert os.path.exists(os.path.join(img_dir, mode, tar))
+
 
 def get_data(img_dir, inp, tar, mode='train', img_options=None):
     check_folder(img_dir, inp, tar, mode)
     return DataReader(img_dir, inp, tar, mode, img_options)
-
-
