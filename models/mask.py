68197ff01a6cb3d281865ca02aa87ca02e71885e
xuhangc <yc17491@umac.mo>
2023-08-25 15:53:07 +0800

    model

---
+++ models/mask.py
@@ -0,0 +1,200 @@
+import numbers
+
+import einops
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def to_3d(x):
+    return einops.rearrange(x, 'b c h w -> b (h w) c')
+
+
+def to_4d(x, h, w):
+    return einops.rearrange(x, 'b (h w) c -> b c h w', h=h, w=w)
+
+
+class BiasFree_LayerNorm(nn.Module):
+    def __init__(self, normalized_shape):
+        super(BiasFree_LayerNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape,)
+        normalized_shape = torch.Size(normalized_shape)
+
+        assert len(normalized_shape) == 1
+
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.normalized_shape = normalized_shape
+
+    def forward(self, x):
+        sigma = x.var(-1, keepdim=True, unbiased=False)
+        return x / torch.sqrt(sigma + 1e-5) * self.weight
+
+
+class WithBias_LayerNorm(nn.Module):
+    def __init__(self, normalized_shape):
+        super(WithBias_LayerNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape,)
+        normalized_shape = torch.Size(normalized_shape)
+
+        assert len(normalized_shape) == 1
+
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.normalized_shape = normalized_shape
+
+    def forward(self, x):
+        mu = x.mean(-1, keepdim=True)
+        sigma = x.var(-1, keepdim=True, unbiased=False)
+        return (x - mu) / torch.sqrt(sigma + 1e-5) * self.weight + self.bias
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim, LayerNorm_type):
+        super(LayerNorm, self).__init__()
+        if LayerNorm_type == 'BiasFree':
+            self.body = BiasFree_LayerNorm(dim)
+        else:
+            self.body = WithBias_LayerNorm(dim)
+
+    def forward(self, x):
+        h, w = x.shape[-2:]
+        return to_4d(self.body(to_3d(x)), h, w)
+
+
+class NextAttentionImplZ(nn.Module):
+    def __init__(self, num_dims, num_heads, bias) -> None:
+        super().__init__()
+        self.num_dims = num_dims
+        self.num_heads = num_heads
+        self.q1 = nn.Conv2d(num_dims, num_dims * 3, kernel_size=1, bias=bias)
+        self.q2 = nn.Conv2d(num_dims * 3, num_dims * 3, kernel_size=3, padding=1, groups=num_dims * 3, bias=bias)
+        self.q3 = nn.Conv2d(num_dims * 3, num_dims * 3, kernel_size=3, padding=1, groups=num_dims * 3, bias=bias)
+
+        self.fac = nn.Parameter(torch.ones(1))
+        self.fin = nn.Conv2d(num_dims, num_dims, kernel_size=1, bias=bias)
+        return
+
+    def forward(self, x):
+        # x: [n, c, h, w]
+        n, c, h, w = x.size()
+        n_heads, dim_head = self.num_heads, c // self.num_heads
+        reshape = lambda x: einops.rearrange(x, "n (nh dh) h w -> (n nh h) w dh", nh=n_heads, dh=dim_head)
+
+        qkv = self.q3(self.q2(self.q1(x)))
+        q, k, v = map(reshape, qkv.chunk(3, dim=1))
+        q = F.normalize(q, dim=-1)
+        k = F.normalize(k, dim=-1)
+
+        # fac = dim_head ** -0.5
+        res = k.transpose(-2, -1)
+        res = torch.matmul(q, res) * self.fac
+        res = torch.softmax(res, dim=-1)
+
+        res = torch.matmul(res, v)
+        res = einops.rearrange(res, "(n nh h) w dh -> n (nh dh) h w", nh=n_heads, dh=dim_head, n=n, h=h)
+        res = self.fin(res)
+
+        return res
+
+
+# Axis-based Multi-head Self-Attention (row and col attention)
+class NextAttentionZ(nn.Module):
+    def __init__(self, num_dims, num_heads=1, bias=True) -> None:
+        super().__init__()
+        assert num_dims % num_heads == 0
+        self.num_dims = num_dims
+        self.num_heads = num_heads
+        self.row_att = NextAttentionImplZ(num_dims, num_heads, bias)
+        self.col_att = NextAttentionImplZ(num_dims, num_heads, bias)
+        return
+
+    def forward(self, x: torch.Tensor):
+        assert len(x.size()) == 4
+
+        x = self.row_att(x)
+        x = x.transpose(-2, -1)
+        x = self.col_att(x)
+        x = x.transpose(-2, -1)
+
+        return x
+
+
+# Dual Gated Feed-Forward Networ
+class FeedForward(nn.Module):
+    def __init__(self, dim, ffn_expansion_factor, bias):
+        super(FeedForward, self).__init__()
+
+        hidden_features = int(dim * ffn_expansion_factor)
+
+        self.project_in = nn.Conv2d(dim, hidden_features * 2, kernel_size=1, bias=bias)
+
+        self.dwconv = nn.Conv2d(hidden_features * 2, hidden_features * 2, kernel_size=3, stride=1, padding=1,
+                                groups=hidden_features * 2, bias=bias)
+
+        self.project_out = nn.Conv2d(hidden_features, dim, kernel_size=1, bias=bias)
+
+    def forward(self, x):
+        x = self.project_in(x)
+        x1, x2 = self.dwconv(x).chunk(2, dim=1)
+        x = F.gelu(x2) * x1 + F.gelu(x1) * x2
+        x = self.project_out(x)
+        return x
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, dim, num_heads=1, ffn_expansion_factor=2.66, bias=True, LayerNorm_type='WithBias'):
+        super(TransformerBlock, self).__init__()
+
+        self.norm1 = LayerNorm(dim, LayerNorm_type)
+        self.attn = NextAttentionZ(dim, num_heads)
+        self.norm2 = LayerNorm(dim, LayerNorm_type)
+        self.ffn = FeedForward(dim, ffn_expansion_factor, bias)
+
+    def forward(self, x):
+        x = x + self.attn(self.norm1(x))
+        x = x + self.ffn(self.norm2(x))
+        return x
+
+
+class ThresholdFormer(nn.Module):
+    def __init__(self, num_trans_blocks):
+        super(ThresholdFormer, self).__init__()
+
+        model = [nn.Conv2d(1, 4, 3, padding=1),
+                 nn.InstanceNorm2d(4),
+                 nn.LeakyReLU(),
+                 nn.Conv2d(4, 16, 3, padding=1),
+                 nn.LeakyReLU()]
+
+        for _ in range(num_trans_blocks):
+            model += [
+                TransformerBlock(dim=16, num_heads=1, ffn_expansion_factor=2.66, bias=False, LayerNorm_type='WithBias')]
+
+        model += [nn.Conv2d(16, 4, 3, padding=1),
+                  nn.LeakyReLU(),
+                  nn.Conv2d(4, 1, 3, padding=1)]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, bin_x):
+        flattened = bin_x.view(-1)
+
+        hist = torch.histc(flattened, bins=256, min=0, max=1)
+
+        pmf = hist / hist.sum()
+        cdf = torch.cumsum(pmf, dim=0)
+
+        cumulative_means = torch.cumsum(pmf * torch.arange(0, 256).to(bin_x.device), dim=0)
+        total_mean = cumulative_means[-1]
+        inter_class_variances = (total_mean * cdf - cumulative_means) ** 2 / (cdf * (1.0 - cdf) + 1e-12)
+        otsu_threshold = torch.argmax(inter_class_variances) / 255
+
+        otsu_mask = torch.ones_like(bin_x, device=bin_x.device)
+        otsu_mask[bin_x > otsu_threshold] = 0
+
+        trans_mask = self.model(otsu_mask)
+
+        res_mask = torch.cat((otsu_mask, trans_mask), dim=1)
+        return res_mask
